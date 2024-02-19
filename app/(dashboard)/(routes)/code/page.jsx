32e7d08d71c496a4c49e9298e8fc443ad14fb547
banuka <banukaramanayaka76@gmail.com>
2024-02-19 21:41:16 +0530

    other:

--- app/(dashboard)/(routes)/code/page.jsx
+++ app/(dashboard)/(routes)/code/page.jsx
@@ -4,6 +4,7 @@ import { Heading } from "@/components/custom/Heading";
 import { useState } from "react";
 import CodePrompt from "./_components/CodePrompt";
 import ReactMarkdown from "react-markdown";
+import MessageBubble from "@/components/custom/MessageBubble";
 export default function CodeGeneration() {
   const [messages, setMessages] = useState([]);
 
@@ -21,24 +22,33 @@ export default function CodeGeneration() {
       consider st5ramnig output like chatgpt
       */}
       {/* <pre>{JSON.stringify(messages, null, 2)}</pre> */}
-      {messages.map((message, i) => (
-        <ReactMarkdown
-          key={i}
-          components={{
-            pre: ({ node, ...props }) => (
-              <div className="overflow-auto w-full my-2 bg-black/20 p-3 rounded-lg">
-                <pre {...props} />
-              </div>
-            ),
-            code: ({ node, ...props }) => (
-              <code className="bg-black/20 rounded-lg w-fit p-1" {...props} />
-            ),
-          }}
-          className="overflow-hidden text-sm  leading-7"
-        >
-          {message.content}
-        </ReactMarkdown>
-      ))}
+      {messages.map((message, i) =>
+        message.role === "user" ? (
+          <MessageBubble
+            key={i}
+            message={message.content}
+            role={message.role}
+            avatar="/avatar.png"
+          />
+        ) : (
+          <ReactMarkdown
+            key={i * 10 + 10}
+            components={{
+              pre: ({ node, ...props }) => (
+                <div className="overflow-auto w-full my-2 bg-black/20 p-3 rounded-lg">
+                  <pre {...props} />
+                </div>
+              ),
+              code: ({ node, ...props }) => (
+                <code className="bg-black/20 rounded-lg w-fit p-1" {...props} />
+              ),
+            }}
+            className="overflow-hidden text-sm  leading-7"
+          >
+            {message.content}
+          </ReactMarkdown>
+        )
+      )}
     </>
   );
 }
