309275507965e4f027b971bad1cd30002fad69ed
banuka <banukaramanayaka76@gmail.com>
2024-02-20 04:55:34 +0530

    other:

--- app/(dashboard)/(routes)/imageReplicate/page.jsx
+++ app/(dashboard)/(routes)/imageReplicate/page.jsx
@@ -9,10 +9,7 @@ import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 
 export default function Conversation() {
-  const [images, setImages] = useState([
-    "https://replicate.delivery/pbxt/Ow8jZAZN5bZjCJtdDKpfxxyMH6VZKBUKRTAqvQQMGefo5xwkA/out-0.png",
-    "https://replicate.delivery/pbxt/yrDDApPBnHZhIRMYsz92T9Km8e8qa5pRM4HiXs8gTgkaeYYSA/out-1.png",
-  ]);
+  const [images, setImages] = useState([]);
 
   return (
     <>
