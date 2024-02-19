a005d0e28b713a315d69daba4f68f40ce64b0b72
banuka <banukaramanayaka76@gmail.com>
2024-02-20 04:54:46 +0530

    other:

--- app/(dashboard)/(routes)/image/page.jsx
+++ app/(dashboard)/(routes)/image/page.jsx
@@ -3,7 +3,11 @@
 import { Heading } from "@/components/custom/Heading";
 import ImagePrompt from "./_components/ImagePrompt";
 import { useState } from "react";
-import { Image } from "lucide-react";
+
+import { Image as LucidImage } from "lucide-react";
+import Image from "next/image";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 export default function Conversation() {
   const [images, setImages] = useState([]);
@@ -13,20 +17,39 @@ export default function Conversation() {
       <Heading
         title="Image Generation"
         description="something descriptive"
-        icon={Image}
+        icon={LucidImage}
         iconColor="text-pink-500"
         bgColor="bg-pink-700/10"
       />
       <h1>dalle 3 free tier only allowes 1 image and resolution 1024 only</h1>
       <h1>dalle 2free tier only allowes 5 image per min</h1>
       <ImagePrompt images={images} setImages={setImages} />
+
       {/* TODO add conversaation style message output 
       consider st5ramnig output like chatgpt
       */}
-      {/* <pre>{JSON.stringify(images, null, 2)}</pre> */}
-      {images.map((image, i) => (
-        <img key={i} src={image.url} alt="horse" />
-      ))}
+
+      <div className="grid grid-cols-1 md:grid-cols-2  gap-4 mt-8 w-[400px] mx-auto">
+        {images.map((image, i) => (
+          <Card key={i * 10 + 10} className="rounded-lg overflow-hidden ">
+            <CardContent className="p-0 pb-6">
+              <div className="relative aspect-square ">
+                <Image
+                  alt="image"
+                  fill
+                  src={image.url}
+                  className="cursor-pointer"
+                  onClick={() => console.log("click")}
+                />
+              </div>
+            </CardContent>
+            <CardFooter className="justify-center">
+              <Button>Download</Button>
+              <Button variant="ghost">Save</Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </>
   );
 }
--- app/(dashboard)/(routes)/imageReplicate/page.jsx
+++ app/(dashboard)/(routes)/imageReplicate/page.jsx
@@ -6,9 +6,13 @@ import { useState } from "react";
 import { Image as LucidImage } from "lucide-react";
 import Image from "next/image";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 export default function Conversation() {
-  const [images, setImages] = useState([]);
+  const [images, setImages] = useState([
+    "https://replicate.delivery/pbxt/Ow8jZAZN5bZjCJtdDKpfxxyMH6VZKBUKRTAqvQQMGefo5xwkA/out-0.png",
+    "https://replicate.delivery/pbxt/yrDDApPBnHZhIRMYsz92T9Km8e8qa5pRM4HiXs8gTgkaeYYSA/out-1.png",
+  ]);
 
   return (
     <>
@@ -19,21 +23,30 @@ export default function Conversation() {
         iconColor="text-pink-500"
         bgColor="bg-pink-700/10"
       />
+
       <ImagePrompt images={images} setImages={setImages} />
+
       {/* TODO add conversaation style message output 
       consider st5ramnig output like chatgpt
       */}
-      {/* <pre>{JSON.stringify(images, null, 2)}</pre> */}
-      <div className="grid grid-cols-1 md:grid-cols-2  gap-4 mt-8">
+
+      <div className="grid grid-cols-1 md:grid-cols-2  gap-4 mt-8 w-[400px] mx-auto">
         {images.map((image, i) => (
-          <Card key={i * 10 + 10} className="rounded-lg overflow-hidden">
-            <CardContent>
-              <div className="relative aspect-square">
-                <Image alt="image" fill src={image} />
+          <Card key={i * 10 + 10} className="rounded-lg overflow-hidden ">
+            <CardContent className="p-0 pb-6">
+              <div className="relative aspect-square ">
+                <Image
+                  alt="image"
+                  fill
+                  src={image}
+                  className="cursor-pointer"
+                  onClick={() => console.log("click")}
+                />
               </div>
             </CardContent>
-            <CardFooter>
-              <p className="p-2">download button</p>
+            <CardFooter className="justify-center">
+              <Button>Download</Button>
+              <Button variant="ghost">Save</Button>
             </CardFooter>
           </Card>
         ))}
