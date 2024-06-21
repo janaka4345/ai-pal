b7ff86db14f440675c2e0c3fd7b6dca851dd3692
Janaka dev <janakchamantha12@gmail.com>
2024-06-21 14:47:07 +0530

    other:misc

--- components/Skeletons/LoadingCodeSkeleton.jsx
+++ components/Skeletons/LoadingCodeSkeleton.jsx
@@ -1,7 +1,6 @@
 export default function LoadingCodeSkeleton() {
     return (
-        <div className="mt-2 w-[60dvw] max-w-sm animate-pulse rounded-e-xl rounded-es-xl border-gray-200 bg-red-100 p-4">
-            {/* className=" text-sm flex flex-col gap-1 leading-7 mb-6" */}
+        <div className="mt-2 max-w-sm animate-pulse">
             <div className="mb-4 h-2 w-48 rounded-full bg-gray-500"></div>
             <div className="mb-4 h-2 w-40 rounded-full bg-gray-500"></div>
             <div className="mb-4 h-2 w-40 rounded-full bg-gray-500"></div>
--- components/custom/SideBar.jsx
+++ components/custom/SideBar.jsx
@@ -26,14 +26,14 @@ const routes = [
     {
         label: 'Conversation',
         icon: MessageSquare,
-        href: '/conversation',
+        href: '/conversation/gemini',
         color: 'text-violet-500',
     },
     {
         label: 'Image Generation',
         icon: ImageIcon,
-        color: 'text-pink-700',
-        href: '/image-gen',
+        color: 'text-pink-700/replicate',
+        href: '/image-gen/replicate',
     },
     // {
     //   label: "Replicate Image Generation",
@@ -57,7 +57,7 @@ const routes = [
         label: 'Code Generation',
         icon: Code,
         color: 'text-green-700',
-        href: '/code',
+        href: '/code/gemini',
     },
     {
         label: 'Settings',
@@ -69,46 +69,35 @@ export default function SideBar() {
     const pathname = usePathname()
     return (
         <>
-            <div className="flex h-full flex-col space-y-4 bg-primary py-4 text-white">
-                <div className="flex-1 px-3 py-2">
-                    <Link
-                        href="/"
-                        className="mb-14 flex flex-col-reverse gap-2 pl-3 md:flex-row md:items-center md:gap-3"
-                    >
-                        <div className="relative mx-auto mr-4 h-8 w-8 md:mx-0">
-                            <Image fill alt="Logo" src="/logo-sass.png" />
-                        </div>
-                        {/* TODO name Changeks */}
-                        <h1 className={cn('text-2xl font-bold')}>SAAS</h1>
-                    </Link>
-                    <div className="space-y-1">
-                        {routes.map((route) => (
-                            <Link
-                                key={route.href}
-                                href={route.href}
-                                className={cn(
-                                    'group flex w-full cursor-pointer justify-start rounded-lg p-3 text-sm font-medium transition hover:bg-white/10 hover:text-white',
-                                    pathname === route.href
-                                        ? 'bg-white/10 text-white'
-                                        : 'text-zinc-400'
-                                )}
-                            >
-                                <div className="flex flex-1 items-center">
-                                    <route.icon
-                                        className={cn(
-                                            'mx-auto mr-3 h-8 w-8 md:mx-0 md:h-5 md:w-5',
-                                            route.color
-                                        )}
-                                    />
-                                    <span className="hidden md:ml-2 md:flex">
-                                        {route.label}
-                                    </span>
-                                </div>
-                            </Link>
-                        ))}
-                    </div>
+            <div className="h-full w-full space-y-4 bg-primary py-4 text-white">
+                <div className="flex h-full flex-col gap-1">
+                    {routes.map((route) => (
+                        <Link
+                            key={route.href}
+                            href={route.href}
+                            className={cn(
+                                'w-full cursor-pointer rounded-lg p-3 text-sm font-medium text-zinc-400 transition hover:bg-white/10 hover:text-white',
+                                {
+                                    'mt-auto': route.href === '/settings',
+                                    'bg-white/10 text-white':
+                                        pathname === route.href,
+                                }
+                            )}
+                        >
+                            <div className="flex flex-1 items-center">
+                                <route.icon
+                                    className={cn(
+                                        'mx-auto mr-3 h-8 w-8 md:mx-0 md:h-5 md:w-5',
+                                        route.color
+                                    )}
+                                />
+                                <span className="hidden md:ml-2 md:flex">
+                                    {route.label}
+                                </span>
+                            </div>
+                        </Link>
+                    ))}
                 </div>
-                {/* <FreeCounter apiLimitCount={apiLimitCount} isPro={isPro} /> */}
             </div>
         </>
     )
