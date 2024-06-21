01b0faf98c09cea82b68f72a27294b64a280f4a1
Janaka dev <janakchamantha12@gmail.com>
2024-06-21 14:40:12 +0530

    other:protected routes added

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 export { default } from 'next-auth/middleware'
 
 //add protected routes here
-export const config = { matcher: ['/authtestroute'] }
+export const config = { matcher: ['/dashboard', '/conversation', '/image-gen', '/video', '/music', '/code'] }
 // "/dashboard",
