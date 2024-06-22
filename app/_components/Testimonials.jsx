e5fe7ae9a859f4fc0661e6aedc073e18132728b9
Janaka dev <janakchamantha12@gmail.com>
2024-06-22 11:36:52 +0530

    other:custom site updated

--- app/_components/Testimonials.jsx
+++ app/_components/Testimonials.jsx
@@ -2,38 +2,37 @@ export default function Testimonials() {
     return (
         <section className="bg-white">
             <div className="mx-auto max-w-screen-xl px-4 py-8 text-center lg:px-6 lg:py-16">
-                <div className="mx-auto max-w-screen-sm">
+                <div className="mx-auto max-w-screen-md">
                     <h2 className="mb-4 text-4xl font-extrabold tracking-tight text-gray-900">
-                        Testimonials
+                        Endorsements
                     </h2>
                     <p className="mb-8 font-light text-gray-500 sm:text-xl lg:mb-16">
-                        Explore the whole collection of open-source web
-                        components and elements built with the utility classes
-                        from Tailwind
+                        Our AI platform combines powerful image / video / music
+                        generation, code creation, and interactive chat, all in
+                        one place. Bring your ideas to life, streamline your
+                        workflow, and unleash your creative potential.
                     </p>
                 </div>
                 <div className="mb-8 grid lg:mb-12 lg:grid-cols-2">
                     <figure className="flex flex-col items-center justify-center border-b border-gray-200 bg-gray-50 p-8 text-center md:p-12 lg:border-r">
                         <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500">
                             <h3 className="text-lg font-semibold text-gray-900">
-                                Speechless with how easy this was to integrate
+                                This AI platform is a game-changer
                             </h3>
                             <p className="my-4">
-                                "I recently got my hands on Flowbite Pro, and
-                                holy crap, I'm speechless with how easy this was
-                                to integrate within my application. Most
-                                templates are a pain, code is scattered, and
-                                near impossible to theme.
+                                "Struggling to bring your ideas to life? This AI
+                                platform is a game-changer.
                             </p>
                             <p className="my-4">
-                                Flowbite has code in one place and I'm not
-                                joking when I say it took me a matter of minutes
-                                to copy the code, customise it and integrate
-                                within a Laravel + Vue application.
+                                Forget wrestling with scattered code or
+                                frustrating design tools. This ai tool combines
+                                powerful image generation, code creation, and
+                                interactive chat - all in one intuitive
+                                platform.
                             </p>
                             <p className="my-4">
-                                If you care for your time, I hands down would go
-                                with this."
+                                I went from concept to code in minutes - no
+                                joke!"
                             </p>
                         </blockquote>
                         <figcaption className="flex items-center justify-center space-x-3">
@@ -45,7 +44,7 @@ export default function Testimonials() {
                             <div className="space-y-0.5 text-left font-medium">
                                 <div>Bonnie Green</div>
                                 <div className="text-sm font-light text-gray-500">
-                                    Developer at Open AI
+                                    Developer at Spotify
                                 </div>
                             </div>
                         </figcaption>
@@ -56,18 +55,20 @@ export default function Testimonials() {
                                 Solid foundation for any project
                             </h3>
                             <p className="my-4">
-                                "FlowBite provides a robust set of design tokens
-                                and components based on the popular Tailwind CSS
-                                framework. From the most used UI components like
-                                forms and navigation bars to the whole app
-                                screens designed both for desktop and mobile,
-                                this UI kit provides a solid foundation for any
-                                project.
+                                "Unleash creativity instantly with this AI
+                                platform. Go from blank page to brilliant
+                                results in minutes.
                             </p>
                             <p className="my-4">
-                                Designing with Figma components that can be
-                                easily translated to the utility classes of
-                                Tailwind CSS is a huge timesaver!"
+                                This intuitive interface combines powerful image
+                                generation, code creation, and interactive chat
+                                - all seamlessly integrated.
+                            </p>
+                            <p className="my-4">
+                                Skip the tedious setup and scattered tools. Our
+                                platform provides a solid foundation for any
+                                project, empowering you to bring your ideas to
+                                life faster than ever"
                             </p>
                         </blockquote>
                         <figcaption className="flex items-center justify-center space-x-3">
@@ -79,7 +80,7 @@ export default function Testimonials() {
                             <div className="space-y-0.5 text-left font-medium">
                                 <div>Roberta Casas</div>
                                 <div className="text-sm font-light text-gray-500">
-                                    Lead designer at Dropbox
+                                    Lead designer at Google
                                 </div>
                             </div>
                         </figcaption>
@@ -87,23 +88,24 @@ export default function Testimonials() {
                     <figure className="flex flex-col items-center justify-center border-b border-gray-200 bg-gray-50 p-8 text-center md:p-12 lg:border-b-0 lg:border-r">
                         <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500">
                             <h3 className="text-lg font-semibold text-gray-900">
-                                Mindblowing workflow and variants
+                                Mindblown by the possibilities!
                             </h3>
                             <p className="my-4">
-                                "As someone who mainly designs in the browser,
-                                I've been a casual user of Figma, but as soon as
-                                I saw and started playing with FlowBite my mind
-                                was 🤯.
+                                "I used to spend hours wrestling with complex
+                                creative tasks. This AI platform is a
+                                revelation.
                             </p>
                             <p className="my-4">
-                                Everything is so well structured and simple to
-                                use (I've learnt so much about Figma by just
-                                using the toolkit).
+                                Everything is so well-structured and intuitive -
+                                I felt like a pro in minutes! The ability to
+                                generate images, code, and have interactive
+                                chats within one platform is simply mindblowing.
                             </p>
                             <p className="my-4">
-                                Aesthetically, the well designed components are
-                                beautiful and will undoubtedly level up your
-                                next application."
+                                It's not just beautiful design components, it's
+                                a whole new way to create. This AI website is
+                                like having a creative superpower in your
+                                pocket!"
                             </p>
                         </blockquote>
                         <figcaption className="flex items-center justify-center space-x-3">
@@ -115,7 +117,7 @@ export default function Testimonials() {
                             <div className="space-y-0.5 text-left font-medium">
                                 <div>Jese Leos</div>
                                 <div className="text-sm font-light text-gray-500">
-                                    Software Engineer at Facebook
+                                    Software Engineer at Amazone
                                 </div>
                             </div>
                         </figcaption>
@@ -123,17 +125,24 @@ export default function Testimonials() {
                     <figure className="flex flex-col items-center justify-center border-gray-200 bg-gray-50 p-8 text-center md:p-12">
                         <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500">
                             <h3 className="text-lg font-semibold text-gray-900">
-                                Efficient Collaborating
+                                Effortless Collaboration Unleashed!
                             </h3>
                             <p className="my-4">
-                                "This is a very complex and beautiful set of
-                                elements. Under the hood it comes with the best
-                                things from 2 different worlds: Figma and
-                                Tailwind.
+                                "This AI platform is a powerful bridge between
+                                creative minds. It's like having the best of two
+                                worlds - seamless collaboration and cutting-edge
+                                AI assistance.
                             </p>
                             <p className="my-4">
-                                You have many examples that can be used to
-                                create a fast prototype for your team."
+                                Forget siloed workflows and communication
+                                struggles. Brainstorm with an AI assistant,
+                                generate stunning visuals, and code together in
+                                real-time.
+                            </p>
+                            <p className="my-4">
+                                This platform provides everything you need to
+                                create groundbreaking projects as a team,
+                                efficiently and effortlessly."
                             </p>
                         </blockquote>
                         <figcaption className="flex items-center justify-center space-x-3">
@@ -145,19 +154,16 @@ export default function Testimonials() {
                             <div className="space-y-0.5 text-left font-medium">
                                 <div>Joseph McFall</div>
                                 <div className="text-sm font-light text-gray-500">
-                                    CTO at Google
+                                    CTO at Mailchimp
                                 </div>
                             </div>
                         </figcaption>
                     </figure>
                 </div>
                 <div className="text-center">
-                    <a
-                        href="#"
-                        className="mb-2 mr-2 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200"
-                    >
+                    <button className="mb-2 mr-2 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200">
                         Show more...
-                    </a>
+                    </button>
                 </div>
             </div>
         </section>
