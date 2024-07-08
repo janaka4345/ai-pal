8ccb18aca9b314a138b9d50d1226c913b305e85d
Janaka dev <janakchamantha12@gmail.com>
2024-07-08 17:51:33 +0530

    feat:user auth form added

--- app/auth/_components/UserAuthForm.jsx
+++ app/auth/_components/UserAuthForm.jsx
@@ -1,69 +1,77 @@
 'use client'
 
-import { cn } from '@/lib/utils'
-// import { Icons } from "@/components/icons"
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+
 import { Button } from '@/components/ui/button'
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { useState } from 'react'
+
+const formSchema = z.object({
+    username: z.string().min(2).max(50),
+    password: z.string().min(2).max(50),
+})
 
 export function UserAuthForm({ className, ...props }) {
-    const [isLoading, setIsLoading] = useState(false)
-
-    async function onSubmit(event) {
-        event.preventDefault()
-        setIsLoading(true)
-
-        setTimeout(() => {
-            setIsLoading(false)
-        }, 3000)
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: '',
+            password: '',
+        },
+    })
+    function onSubmit(values) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
     }
 
     return (
-        <div className={cn('grid gap-6', className)} {...props}>
-            <form onSubmit={onSubmit}>
-                <div className="grid gap-2">
-                    <div className="grid gap-1">
-                        <Label className="sr-only" htmlFor="email">
-                            Email
-                        </Label>
-                        <Input
-                            id="email"
-                            placeholder="name@example.com"
-                            type="email"
-                            autoCapitalize="none"
-                            autoComplete="email"
-                            autoCorrect="off"
-                            disabled={isLoading}
-                        />
-                    </div>
-                    <Button disabled={isLoading}>
-                        {/* {isLoading && ( 
-             <Icons.spinner className="mr-2 h-4 w-4 animate-spin" /> 
-             )}
-             */}
-                        Sign In with Email
-                    </Button>
-                </div>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder="shadcn" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem className="mt-0">
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input
+                                    type="password"
+                                    placeholder="*********"
+                                    {...field}
+                                />
+                            </FormControl>
+
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Sign In with Email</Button>
             </form>
-            <div className="relative">
-                <div className="absolute inset-0 flex items-center">
-                    <span className="w-full border-t" />
-                </div>
-                <div className="relative flex justify-center text-xs uppercase">
-                    <span className="bg-background px-2 text-muted-foreground">
-                        Or continue with
-                    </span>
-                </div>
-            </div>
-            <Button variant="outline" type="button" disabled={isLoading}>
-                {/* {isLoading ? (
-           <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-        ) : (
-           <Icons.gitHub className="mr-2 h-4 w-4" />
-        )}{" "} */}
-                GitHub
-            </Button>
-        </div>
+        </Form>
     )
 }
