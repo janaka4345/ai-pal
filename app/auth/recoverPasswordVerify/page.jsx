d7f83d01890ce434a3af9d2430dddba22ed9a982
Janaka dev <janakchamantha12@gmail.com>
2024-07-15 23:29:15 +0530

    feat:password recovery component and form added

---
+++ app/auth/recoverPasswordVerify/page.jsx
@@ -0,0 +1,11 @@
+import RecoverPasswordVerifyForm from '../_components/RecoverPasswordVerifyForm'
+export default function page() {
+    return (
+        <div className="g-6 mx-auto flex flex-col">
+            <div className="mx-auto flex h-[60svh] w-[350px] flex-col justify-around">
+                <h1>Verify your Password reset request</h1>
+                <RecoverPasswordVerifyForm />
+            </div>
+        </div>
+    )
+}
