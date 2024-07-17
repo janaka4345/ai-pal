d7dc946366900f6a95c998f13288fa637f11857f
Janaka dev <janakchamantha12@gmail.com>
2024-07-17 19:43:09 +0530

    other:protected routes added to middleware

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
-export { default } from 'next-auth/middleware'
+export { default } from 'next-auth/middleware';
+
 
 //add protected routes here
-export const config = { matcher: ['/dashboard', '/conversation', '/image-gen', '/video', '/music', '/code'] }
+export const config = { matcher: ['/dashboard', '/conversation', '/image-gen', '/video', '/music', '/code', '/api/token/tokenCount', '/api/codeGemini', '/api/codeOpenai', '/api/conversation', '/api/conversationGemini', '/api/doenloadFiles', '/api/image', '/api/imageReplicate'] }
 // "/dashboard",
