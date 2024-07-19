e4f6cb67d5651c7c31480a6ba2eab4db373cb4d7
Janaka dev <janakchamantha12@gmail.com>
2024-07-19 22:14:10 +0530

    style: style changes to hero component

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -3,27 +3,30 @@ import Companies from './Companies'
 
 export default function Hero() {
     return (
-        <section id="hero" className="">
-            <div className="mx-auto flex h-[calc(100dvh-10dvh)] max-w-screen-xl flex-col px-4 py-8 text-center lg:py-16">
-                <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-secondary-foreground md:text-5xl lg:text-6xl">
-                    <span className="bg-gradient-to-r from-cyan-500 to-primary bg-clip-text text-6xl text-transparent lg:text-8xl">
+        <section id="hero">
+            <div className="mx-auto flex h-[calc(100dvh-10dvh)] w-fit flex-col px-4 py-8 text-center lg:py-16">
+                <h1 className="mb-4 font-extrabold leading-none tracking-tight">
+                    <span className="bg-gradient-to-r from-cyan-500 to-primary bg-clip-text text-5xl text-transparent md:text-6xl lg:text-8xl">
                         Create.
                     </span>
-                    <span className="bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text text-6xl text-transparent lg:text-8xl">
+                    <br className="sm:hidden" />
+                    <span className="bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text text-5xl text-transparent md:text-6xl lg:text-8xl">
                         Code.
                     </span>
                     <br />
-                    <span className="bg-gradient-to-r from-indigo-500 to-pink-500 bg-clip-text text-6xl text-transparent lg:text-8xl">
+                    <span className="bg-gradient-to-r from-indigo-500 to-pink-500 bg-clip-text text-5xl text-transparent md:text-6xl lg:text-8xl">
                         Converse.
                     </span>
                     <br />
-                    <span className="">All-in-one AI toolkit</span>
                 </h1>
-                <p className="my-8 text-lg font-normal text-secondary-foreground sm:px-16 lg:px-48 lg:text-xl">
+                <p className="text-4xl font-extrabold leading-none tracking-tight text-secondary-foreground md:text-5xl">
+                    All-in-one AI toolkit
+                </p>
+                <p className="my-8 text-lg font-normal text-secondary-foreground sm:px-16 lg:px-48 lg:text-2xl">
                     Stuck on a design? Struggling with code? Need a creative
                     chat buddy? We've got you covered.
                 </p>
-                <div className="mt-16 flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+                <div className="mt-auto flex flex-col space-y-4 sm:flex-row sm:justify-center">
                     <Link
                         href="/dashboard"
                         className="mt-auto inline-flex items-center justify-center rounded-lg bg-primary px-5 py-3 text-center text-base font-medium text-white hover:bg-primary focus:ring-4 focus:ring-primary"
