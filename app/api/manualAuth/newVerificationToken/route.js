db63df92d3eca276f8eeccb3e8dd5bfdac318b19
Janaka dev <janakchamantha12@gmail.com>
2024-08-06 23:02:00 +0530

    feat:smpt email send with react email

--- app/api/manualAuth/newVerificationToken/route.js
+++ app/api/manualAuth/newVerificationToken/route.js
@@ -1,6 +1,9 @@
+import VerificationEmail from "@/emails/VerificationEmail";
 import { generateToken } from "@/lib/generateToken";
+import { transporter } from "@/lib/nodemailer";
 import prisma from "@/lib/prismaClient";
 import { verificationTokenSchema } from "@/lib/shema";
+import { render } from "@react-email/components";
 import { NextResponse } from "next/server";
 
 export async function POST(req, res) {
@@ -29,7 +32,7 @@ export async function POST(req, res) {
 
     // console.log({ token, expires });
     // console.log(existingToken?.token);
-
+    const emailHtml = render(<VerificationEmail validationCode={token} />);
     try {
         await prisma.verificationToken.updateMany({
             where: {
@@ -40,7 +43,17 @@ export async function POST(req, res) {
                 expires
             }
         })
-        return NextResponse.json({ success: 'new verification token created successfuly' })
+
+        await transporter.sendMail({
+            from: 'J_ Keyy<ashnazg1212@gmail.com>',
+            to: email,
+            subject: "Verify your Email with AI-PAL", // Subject line
+            text: "use the verificatio ntoken below tp verify your Email with AI-PAL", // plain text body
+            html: emailHtml, // html body
+        })
+
+
+        return NextResponse.json({ success: 'new verification token has been sent successfully.' })
     } catch (error) {
         console.log(error);
         return NextResponse.json({ error: 'something went wrong' })
