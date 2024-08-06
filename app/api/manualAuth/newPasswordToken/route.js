7d1cae5b2c99e5bd16b11ae00005613a56406711
Janaka dev <janakchamantha12@gmail.com>
2024-08-06 22:59:16 +0530

    feat: smpt email sending added

--- app/api/manualAuth/newPasswordToken/route.js
+++ app/api/manualAuth/newPasswordToken/route.js
@@ -1,6 +1,9 @@
+import VerificationEmail from "@/emails/VerificationEmail";
 import { generateToken } from "@/lib/generateToken";
+import { transporter } from "@/lib/nodemailer";
 import prisma from "@/lib/prismaClient";
 import { passwordTokenSchema } from "@/lib/shema";
+import { render } from "@react-email/components";
 import { NextResponse } from "next/server";
 
 export async function POST(req, res) {
@@ -29,7 +32,7 @@ export async function POST(req, res) {
 
     // console.log({ token, expires });
     // console.log(existingToken?.token);
-
+    const emailHtml = render(<VerificationEmail validationCode={token} />);
     try {
         await prisma.passwordToken.updateMany({
             where: {
@@ -40,7 +43,16 @@ export async function POST(req, res) {
                 expires
             }
         })
-        return NextResponse.json({ success: 'new password token created successfuly' })
+
+        await transporter.sendMail({
+            from: 'J_ Keyy<ashnazg1212@gmail.com>',
+            to: email,
+            subject: "Verify your Email with AI-PAL", // Subject line
+            text: "use the verificatio ntoken below tp verify your Email with AI-PAL", // plain text body
+            html: emailHtml, // html body
+        })
+
+        return NextResponse.json({ success: 'new password token has been sent successfuly' })
     } catch (error) {
         console.log(error);
         return NextResponse.json({ error: 'something went wrong' })
